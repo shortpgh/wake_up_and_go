328078121df34795510463287b32f16d5536eded
nik mihalick <nik@shortpgh.com>
2014-11-01 11:36:26 -0400

    day 1: updated the journal script to put files in the journal dir. also make it auto-add the file to git.

--- journal/new.sh
+++ journal/new.sh
@@ -1,1 +1,5 @@
-echo "=Day "`ls | wc -w`": " `date "+%Y-%m-%d\n"` >> `date "+%Y%m%d"`.md
+#!/bin/bash
+putpath=`dirname "${BASH_SOURCE[0]}"`/
+filename=$putpath`date "+%Y%m%d"`.md
+echo "=Day "`ls | wc -w`": " `date "+%Y-%m-%d\n"` >> $filename
+git add $filename
